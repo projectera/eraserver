5bdfe980679099fb39a3e04c3125d36569fb92a5
Max Maton <thexa4@gmail.com>
2012-10-24 18:39:36 +0200

    Added constructor comment

--- EraS/Topography/Service.cs
+++ EraS/Topography/Service.cs
@@ -14,10 +14,21 @@ namespace EraS.Topography
         /// <summary>
         /// The service name
         /// </summary>
-        public String Name { get; protected set; }
+        public String Name { get; set; }
         /// <summary>
         /// The unique identifier for the service
         /// </summary>
         public String Identifier { get; protected set; }
+
+        /// <summary>
+        /// Creates a new Service instance
+        /// </summary>
+        /// <param name="server">The server it belongs to</param>
+        /// <param name="identifier">The unique identifier (hash)</param>
+        public Service(Server server, String identifier)
+        {
+            Server = server;
+            Identifier = identifier;
+        }
     }
 }
