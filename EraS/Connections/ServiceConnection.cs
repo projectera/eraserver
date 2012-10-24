0328bfabe689572d3c417da827da299a402829cf
Max Maton <thexa4@gmail.com>
2012-10-24 18:37:10 +0200

    Created mock of ServiceConnection

--- EraS/Connections/ServiceConnection.cs
+++ EraS/Connections/ServiceConnection.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EraS.Topography;
 
 namespace EraS.Connections
 {
     class ServiceConnection
     {
+        public Service Service { get; protected set; }
 
+        public ServiceConnection(Service service)
+        {
+            Service = service;
+        }
+
+        public void SendMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Kill()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
