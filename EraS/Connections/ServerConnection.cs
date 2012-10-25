4c4eebc52d4eaf65336340539bc08affa726bb4b
Max Maton <thexa4@gmail.com>
2012-10-26 00:37:12 +0200

    Added fence mock

--- EraS/Connections/ServerConnection.cs
+++ EraS/Connections/ServerConnection.cs
@@ -7,5 +7,13 @@ namespace EraS.Connections
 {
     class ServerConnection
     {
+        /// <summary>
+        /// Fences the server
+        /// This will result in a restart of EraS (while killing all services)
+        /// </summary>
+        public void Fence()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
