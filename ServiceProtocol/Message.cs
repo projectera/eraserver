d28fcc2b0882a0cbd9e01c479e94009dd8688094
Max Maton <thexa4@gmail.com>
2012-10-26 00:39:13 +0200

    Added Message class

---
+++ ServiceProtocol/Message.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+
+namespace ServiceProtocol
+{
+    /// <summary>
+    /// Represents a message packet
+    /// </summary>
+    class Message
+    {
+        /// <summary>
+        /// The network package
+        /// </summary>
+        public NetBuffer Packet { get; protected set; }
+        /// <summary>
+        /// The type of message
+        /// </summary>
+        public MessageType Type { get; protected set; }
+        /// <summary>
+        /// The destination of the message
+        /// </summary>
+        public String Destination { get; protected set; }
+
+        /// <summary>
+        /// Creates a new outgoing message
+        /// </summary>
+        /// <param name="message">The network packet</param>
+        /// <param name="type">The message type</param>
+        /// <param name="destination">The destination</param>
+        public Message(NetOutgoingMessage message, MessageType type, String destination)
+        {
+            Packet = message;
+            Type = type;
+            Packet.Write((Byte)Type);
+            Destination = destination;
+            Packet.Write(Destination);
+        }
+
+        /// <summary>
+        /// Creates a new incoming message
+        /// </summary>
+        /// <param name="message">The incoming packet</param>
+        public Message(NetIncomingMessage message)
+        {
+            Packet = message;
+            Type = (MessageType)Packet.ReadByte();
+            Destination = Packet.ReadString();
+        }
+    }
+}
---
+++ ServiceProtocol/MessageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceProtocol
+{
+    enum MessageType : byte
+    {
+        EraS = 0,
+        Internal = 1,
+        Service = 2,
+    }
+}
