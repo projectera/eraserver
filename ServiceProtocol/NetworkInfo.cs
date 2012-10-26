1ea5cdc3ac09b5df3f80724f902a4b6545626aca
Max Maton <thexa4@gmail.com>
2012-10-26 02:29:27 +0200

    Added example question to EraS

--- ServiceProtocol/NetworkInfo.cs
+++ ServiceProtocol/NetworkInfo.cs
@@ -5,7 +5,35 @@ using System.Text;
 
 namespace ServiceProtocol
 {
+    /// <summary>
+    /// Gets info from the connected server
+    /// </summary>
     class NetworkInfo
     {
+        /// <summary>
+        /// The socket used to connect to the server
+        /// </summary>
+        public ServiceClient Client { get; protected set; }
+
+        /// <summary>
+        /// Creates a new NetworkInfo object
+        /// </summary>
+        /// <param name="client">The client to use</param>
+        public NetworkInfo(ServiceClient client)
+        {
+            Client = client;
+        }
+
+        /// <summary>
+        /// Asks the identifier of the connected server
+        /// </summary>
+        /// <returns>The identifier of the connected server</returns>
+        public string GetServerIdentifier()
+        {
+            var m = Client.CreateQuestion(MessageType.EraS, "Self");
+            m.Packet.Write("GetIdentifier");
+            var res = Client.AskReliableQuestion(m);
+            return res.Packet.ReadString();
+        }
     }
 }
