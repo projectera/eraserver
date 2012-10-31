c9398afc0bdd788780061037f3e906d427570ae3
Max Maton <thexa4@gmail.com>
2012-10-31 23:41:17 +0100

    Forgot to include Component

---
+++ EraS/MessageHandlers/ErasComponents/StatisticsComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EraS.Services;
+
+namespace EraS.MessageHandlers.ErasComponents
+{
+    class StatisticsComponent : DefaultComponent
+    {
+        public StatisticsComponent() : base("Statistics")
+        {
+            //TODO: move functions from StatisticsService to here
+            foreach (var function in StatisticsService.Functions)
+                Functions.Add(function.Key, function.Value);
+        }
+    }
+}
