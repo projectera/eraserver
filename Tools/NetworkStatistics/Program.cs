16bfa4b53352192ad2cff4fef1f7b3d375b72ae5
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-10-31 16:58:50 +0100

    Added new NetworkStatistics Tool
    This will be able to monitor bandwith usuage from/to EraS and its
    services.

---
+++ Tools/NetworkStatistics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceProtocol;
+
+namespace NetworkStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = ServiceClient.Connect("NetworkStatistics");
+            NetworkInfo n = new NetworkInfo(client);
+        }
+    }
+}
