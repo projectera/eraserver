c7a1ef802a31b33327def83a80efcc55d62a0c91
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-05 01:29:48 +0100

    Added public messagetype and descriptions

--- ServiceProtocol/MessageType.cs
+++ ServiceProtocol/MessageType.cs
@@ -7,10 +7,34 @@ namespace ServiceProtocol
 {
     public enum MessageType : byte
     {
+        /// <summary>
+        /// Communication for EraS
+        /// </summary>
         EraS = 0,
+
+        /// <summary>
+        /// 
+        /// </summary>
         Internal = 1,
+
+        /// <summary>
+        /// Communication for service
+        /// </summary>
         Service = 2,
+
+        /// <summary>
+        /// Control communication instances
+        /// </summary>
         Control = 3,
+
+        /// <summary>
+        /// Communication to question origin
+        /// </summary>
         Answer = 4,
+
+        /// <summary>
+        /// Communication to subscribers
+        /// </summary>
+        Public = 5
     }
 }
