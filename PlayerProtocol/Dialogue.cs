cf0b530b6eb0d93d38b873b21af3be223f8ff85b
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-13 15:49:31 +0100

    Added Dialogues (Private Messages)

---
+++ PlayerProtocol/Dialogue.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Lidgren.Network;
+
+namespace PlayerProtocol
+{
+    /// <summary>
+    /// Dialogue
+    /// </summary>
+    [BsonIgnoreExtraElements]
+    public class Dialogue
+    {
+        public const Int32 DialogueSize = 2000;
+
+        /// <summary>
+        /// Dialogue Id
+        /// </summary>
+        [BsonId]
+        public ObjectId Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Active participants
+        /// </summary>
+        [BsonRequired]
+        protected HashSet<ObjectId> Participants
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Messages in the dialogue
+        /// </summary>
+        [BsonRequired]
+        protected List<DialogueMessage> Messages
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Dialogue Creation
+        /// </summary>
+        [BsonIgnore]
+        public DateTime TimeStamp
+        {
+            get
+            {
+                return Id.CreationTime;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ObjectId? FollowUp
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Generates a new Dialogue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="receiver"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Dialogue Generate(ObjectId sender, ObjectId receiver, String message)
+        {
+            DialogueMessage dmsg = DialogueMessage.Generate(sender, message);
+
+            Dialogue result = new Dialogue();
+            result.Id = ObjectId.GenerateNewId();
+            result.Participants = new HashSet<ObjectId>() { receiver, sender };
+            result.Participants.Remove(ObjectId.Empty);
+            result.Messages = new List<DialogueMessage>() { dmsg };
+            result.FollowUp = null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="participants"></param>
+        /// <returns></returns>
+        public static Dialogue Generate(DialogueMessage message, HashSet<ObjectId> participants)
+        {
+            Dialogue result = new Dialogue();
+            result.Id = ObjectId.GenerateNewId();
+            result.Participants = participants;
+            result.Messages = new List<DialogueMessage>() { message };
+            result.FollowUp = null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static Dialogue Generate(DialogueMessage message, List<ObjectId> list)
+        {
+            var participants = new HashSet<ObjectId>();
+            foreach (var participant in list)
+                participants.Add(participant);
+            return Generate(message, participants);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="messages"></param>
+        /// <param name="participants"></param>
+        /// <returns></returns>
+        public static Dialogue Generate(ObjectId id, List<DialogueMessage> messages, HashSet<ObjectId> participants)
+        {
+            Dialogue result = new Dialogue();
+            result.Id = id;
+            result.Participants = participants;
+            result.Messages = messages;
+            result.FollowUp = null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="messages"></param>
+        /// <param name="participants"></param>
+        /// <param name="followUp"></param>
+        /// <returns></returns>
+        public static Dialogue Generate(ObjectId id, List<DialogueMessage> messages, HashSet<ObjectId> participants, ObjectId followUp)
+        {
+            var result = Generate(id, messages, participants);
+            result.FollowUp = followUp;
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public void AddMessage(DialogueMessage message)
+        {
+            lock (this.Messages)
+            {
+                this.Messages.Add(message);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<DialogueMessage> GetMessages()
+        {
+            lock (this.Messages)
+            {
+                return this.Messages.ToList();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<ObjectId> GetParticipants()
+        {
+            lock (this.Participants)
+            {
+                return this.Participants.ToList();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="participant"></param>
+        /// <returns></returns>
+        public Boolean AddParticipant(ObjectId participant)
+        {
+            lock (this.Participants)
+            {
+                return this.Participants.Add(participant);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public NetBuffer Pack(ref NetBuffer msg)
+        {
+            msg.Write(this.Id.ToByteArray()); // 12
+            msg.Write(this.Participants.Count); // 16
+            foreach (var participant in this.Participants)
+                msg.Write(participant.ToByteArray()); // 16 + 12 * x
+            msg.Write(this.Messages.Count()); // 20 + 12 * x
+            msg.Write(this.TimeStamp.ToBinary()); // 28 + 12 * x + 12 * y
+
+            return msg;
+        }
+    }
+}
+
---
+++ PlayerProtocol/DialogueMessage.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Lidgren.Network;
+
+namespace PlayerProtocol
+{
+    /// <summary>
+    /// Dialogue message
+    /// </summary>
+    public class DialogueMessage
+    {
+        /// <summary>
+        /// Message id
+        /// </summary>
+        [BsonRequired]
+        public ObjectId Id
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Sender of message
+        /// </summary>
+        [BsonRequired]
+        public ObjectId Sender
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Contents of message
+        /// </summary>
+        [BsonRequired]
+        public String Contents
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Timestamp of message
+        /// </summary>
+        [BsonIgnore]
+        public DateTime TimeStamp
+        {
+            get
+            {
+                return Id.CreationTime;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [BsonRequired]
+        public IAttachment Attachment
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [BsonRequired]
+        public HashSet<ObjectId> Read
+        {
+            get;
+            private set;
+        }
+
+        [BsonIgnore]
+        public Dialogue Parent
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Attachment interface
+        /// </summary>
+        [BsonDiscriminator(RootClass = true)]
+        [BsonKnownTypes(typeof(AchievementAttachment), typeof(EffortAttachment), typeof(ItemAttachment), typeof(LocationAttachment))]
+        public abstract class IAttachment
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            [BsonId]
+            public ObjectId Id
+            {
+                get;
+                protected set;
+            }
+
+
+            /// <summary>
+            /// Attachment id
+            /// </summary>
+            [BsonRequired]
+            public ObjectId Sender
+            {
+                get;
+                protected set;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [BsonIgnore]
+            public DateTime TimeStamp
+            {
+                get { return Id.CreationTime; }
+            }
+
+            public abstract NetBuffer Pack(ref NetBuffer msg);
+        }
+
+        /// <summary>
+        /// Attachment in message to show an interactables location
+        /// </summary>
+        public class LocationAttachment : IAttachment
+        {
+            private const Byte AttachmentType = 1;
+
+            /// <summary>
+            /// Map id
+            /// </summary>
+            [BsonRequired]
+            public ObjectId MapId
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Map x coord
+            /// </summary>
+            [BsonRequired]
+            public Int32 MapX
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Map y coord
+            /// </summary>
+            [BsonRequired]
+            public Int32 MapY
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Generates location attachment
+            /// </summary>
+            /// <param name="source"></param>
+            /// <returns></returns>
+            public static IAttachment Generate(ObjectId sender, ObjectId mapId, Int32 mapX, Int32 mapY)
+            {
+                LocationAttachment result = new LocationAttachment();
+                result.Id = ObjectId.GenerateNewId();
+                result.Sender = sender;
+                result.MapId = mapId;
+                result.MapX = mapX;
+                result.MapY = mapY;
+
+                return result;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="msg"></param>
+            /// <returns></returns>
+            public override NetBuffer Pack(ref NetBuffer msg)
+            {
+                msg.Write(AttachmentType); // 1
+                msg.Write(this.Id.ToByteArray()); // 13
+                msg.Write(this.MapId.ToByteArray()); // 25
+                msg.Write(this.MapX); // 29
+                msg.Write(this.MapY); // 33
+                msg.Write(this.Sender.ToByteArray()); // 45
+                msg.Write(this.TimeStamp.ToBinary()); // 53
+                return msg;
+            }
+        }
+
+        /// <summary>
+        /// Attachment in message to show an item
+        /// </summary>
+        public class ItemAttachment : IAttachment
+        {
+            private const Byte AttachmentType = 2;
+
+            /// <summary>
+            /// Item id
+            /// </summary>
+            [BsonRequired]
+            public ObjectId ItemId
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Blueprint id
+            /// </summary>
+            public Int32 BlueprintId
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Generates item attachment
+            /// </summary>
+            /// <param name="item"></param>
+            /// <returns></returns>
+            public static IAttachment Generate(ObjectId sender, ObjectId itemId, Int32 blueprintId)
+            {
+                ItemAttachment result = new ItemAttachment();
+                result.Id = ObjectId.GenerateNewId();
+                result.ItemId = itemId;
+                result.Sender = sender;
+                result.BlueprintId = blueprintId;
+
+                return result;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="msg"></param>
+            /// <returns></returns>
+            public override NetBuffer Pack(ref NetBuffer msg)
+            {
+                msg.Write(AttachmentType); // 1
+                msg.Write(this.Id.ToByteArray()); // 13
+                msg.Write(this.BlueprintId); // 17
+                msg.Write(this.Sender.ToByteArray()); // 29
+                msg.Write(this.TimeStamp.ToBinary()); // 37
+                return msg;
+            }
+        }
+
+        /// <summary>
+        /// Attachment in message to show an achievement
+        /// </summary>
+        public class AchievementAttachment : IAttachment
+        {
+            private const Byte AttachmentType = 3;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="msg"></param>
+            /// <returns></returns>
+            public override NetBuffer Pack(ref NetBuffer msg)
+            {
+                msg.Write(AttachmentType); // 1
+                msg.Write(this.Id.ToByteArray()); // 13
+                msg.Write(this.Sender.ToByteArray()); // 25
+                msg.Write(this.TimeStamp.ToBinary()); // 33
+                return msg;
+            }
+        }
+
+        /// <summary>
+        /// Attachment in message to show an effort
+        /// </summary>
+        public class EffortAttachment : IAttachment
+        {
+            private const Byte AttachmentType = 3;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="msg"></param>
+            /// <returns></returns>
+            public override NetBuffer Pack(ref NetBuffer msg)
+            {
+                msg.Write(AttachmentType); // 1
+                msg.Write(this.Id.ToByteArray()); // 13
+                msg.Write(this.Sender.ToByteArray()); // 25
+                msg.Write(this.TimeStamp.ToBinary()); // 33
+                return msg;
+            }
+        }
+
+        /// <summary>
+        /// Generates a new dialog message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static DialogueMessage Generate(ObjectId sender, String message)
+        {
+            DialogueMessage result = new DialogueMessage();
+            result.Id = ObjectId.GenerateNewId();
+            result.Sender = sender;
+            result.Contents = message;
+            result.Read = new HashSet<ObjectId> { sender };
+            result.Read.Remove(ObjectId.Empty);
+            result.Parent = null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Attaches attachment to a message
+        /// </summary>
+        /// <param name="attachment"></param>
+        public void Attach(DialogueMessage.IAttachment attachment)
+        {
+            this.Attachment = attachment;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="receipientId"></param>
+        public Boolean MarkAsRead(ObjectId readerId)
+        {
+            return readerId != ObjectId.Empty && this.Read.Add(readerId);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="readerId"></param>
+        public Boolean MarkAsUnread(ObjectId readerId)
+        {
+            return this.Read.Remove(readerId);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public NetBuffer Pack(ref NetBuffer msg)
+        {
+            msg.Write(this.Id.ToByteArray()); // 12
+            msg.Write(this.Contents); // 12 + x
+            msg.Write(this.Sender.ToByteArray()); // 24 + x
+            msg.Write(this.TimeStamp.ToBinary()); // 32 + x
+            msg.Write(this.Attachment != null); // 33 + x
+            if (this.Attachment != null)
+                this.Attachment.Pack(ref msg); // (33 || 45) + x
+
+            return msg;
+        }
+    }
+}
