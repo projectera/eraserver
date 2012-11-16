fbef2cad5affca12fa61a8c55cfdbaba63187cca
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-16 16:40:43 +0100

    Added serialization options and id

--- Services/InteractableService/Data/ServerInteractable.cs
+++ Services/InteractableService/Data/ServerInteractable.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace ERA.Services.InteractableService.Data
 {
     internal class ServerInteractable
     {
+        [BsonId]
+        public ObjectId Id;
+
+        [BsonRequired]
         public ERA.Protocols.InteractableProtocol.Interactable PublicData { get; set; }
     }
 }
